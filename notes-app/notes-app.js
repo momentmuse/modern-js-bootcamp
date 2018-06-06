ff1bfbe13a0f7df10d31e8c9cb0608447f8ba001
Amy Kirasack <amykirasack@gmail.com>
2018-06-05 23:32:10 -0400

    Practice built-in JS date

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -32,3 +32,25 @@ window.addEventListener('storage', function(e) {
     renderNotes(notes, inputFilters);
     }
 })
+
+//Unix Epoch - January 1 1970 00:00:00
+//negative before, positive after, in miliseconds
+
+const now = new Date()
+const timestamp = now.getTime()
+
+const myDate = new Date(timestamp)
+console.log(myDate.getFullYear())
+
+// console.log(`Year: ${now.getFullYear()}`)
+// console.log(`Month: ${now.getMonth()}`)
+// console.log(`Date: ${now.getDate()}`)
+// console.log(`Hour: ${now.getHours()}`)
+// console.log(`Minute: ${now.getMinutes()}`)
+// console.log(`Second: ${now.getSeconds()}`)
+
+const date1 = new Date('January 12 2012 16:43:12')
+const date2 = new Date('March 23 1995 12:34:01')
+date1.getTime() < date2.getTime() ?
+    console.log(date1.toString())
+    : console.log(date2.toString())

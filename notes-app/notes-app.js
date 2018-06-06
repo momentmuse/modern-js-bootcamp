c63fc3c22512cf9263faa8c6616c601b867819d4
Amy Kirasack <amykirasack@gmail.com>
2018-06-05 23:54:46 -0400

    Practice Moment js library

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -33,24 +33,35 @@ window.addEventListener('storage', function(e) {
     }
 })
 
-//Unix Epoch - January 1 1970 00:00:00
-//negative before, positive after, in miliseconds
+// const now = moment()
+// now.subtract(1, 'year').subtract(20, 'days')
+// console.log(now.format('MMMM Do, YYYY'))
+// console.log(now.fromNow())
+// const nowTimestamp = now.valueOf()
+// console.log(nowTimestamp)
 
-const now = new Date()
-const timestamp = now.getTime()
+const myBday = moment()
+myBday.year(1991).month(1).date(10)
+console.log(myBday.format('MMM D, YYYY'))
 
-const myDate = new Date(timestamp)
-console.log(myDate.getFullYear())
+// //Unix Epoch - January 1 1970 00:00:00
+// //negative before, positive after, in miliseconds
 
-// console.log(`Year: ${now.getFullYear()}`)
-// console.log(`Month: ${now.getMonth()}`)
-// console.log(`Date: ${now.getDate()}`)
-// console.log(`Hour: ${now.getHours()}`)
-// console.log(`Minute: ${now.getMinutes()}`)
-// console.log(`Second: ${now.getSeconds()}`)
+// const now = new Date()
+// const timestamp = now.getTime()
 
-const date1 = new Date('January 12 2012 16:43:12')
-const date2 = new Date('March 23 1995 12:34:01')
-date1.getTime() < date2.getTime() ?
-    console.log(date1.toString())
-    : console.log(date2.toString())
+// const myDate = new Date(timestamp)
+// console.log(myDate.getFullYear())
+
+// // console.log(`Year: ${now.getFullYear()}`)
+// // console.log(`Month: ${now.getMonth()}`)
+// // console.log(`Date: ${now.getDate()}`)
+// // console.log(`Hour: ${now.getHours()}`)
+// // console.log(`Minute: ${now.getMinutes()}`)
+// // console.log(`Second: ${now.getSeconds()}`)
+
+// const date1 = new Date('January 12 2012 16:43:12')
+// const date2 = new Date('March 23 1995 12:34:01')
+// date1.getTime() < date2.getTime() ?
+//     console.log(date1.toString())
+//     : console.log(date2.toString())

338ebc0c23cfbc8818fcdeec76080b63b6b6ec28
Amy Kirasack <amykirasack@gmail.com>
2018-06-06 22:26:31 -0400

    Refactor functions with arrow function format

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -7,12 +7,12 @@ const inputFilters = {
 
 renderTodos(todos, inputFilters);
 
-document.querySelector('#filter-todos-text-input').addEventListener('input', function(e) {
+document.querySelector('#filter-todos-text-input').addEventListener('input', (e) => {
     inputFilters.searchTodos = e.target.value;
     renderTodos(todos, inputFilters);
 });
 
-document.querySelector('#new-todo-form').addEventListener('submit', function(e) {
+document.querySelector('#new-todo-form').addEventListener('submit', (e) => {
     e.preventDefault();
     todos.push({
         id: uuidv4(),
@@ -24,7 +24,7 @@ document.querySelector('#new-todo-form').addEventListener('submit', function(e) 
     e.target.elements.newTodoTextInput.value = '';
 });
 
-document.querySelector('#hide-completed').addEventListener('change', function(e) {
+document.querySelector('#hide-completed').addEventListener('change', (e) => {
     inputFilters.hideCompleted = e.target.checked;
     //if checkbox is checked, hideCompleted = true; display !todo.completed
     //if checkbox is unchecked, hideCompleted = false; display all (todo.completed && !todo.completed; all true)
--- todo-app/todo-functions.js
+++ todo-app/todo-functions.js
@@ -1,5 +1,5 @@
 //Fetch existing todos from localStorage
-const getSavedTodos = function() {
+const getSavedTodos = () => {
     const todosJSON = localStorage.getItem('todos')
 
     if (todosJSON !== null) {
@@ -10,15 +10,13 @@ const getSavedTodos = function() {
 }
 
 //Save todos to localStorage
-const saveTodos = function(todos) {
+const saveTodos = (todos) => {
     localStorage.setItem('todos', JSON.stringify(todos));
 }
 
 //Remove todo by id
-const removeTodo = function(id) {
-    const todoIndex = todos.findIndex(function (todo) {
-        return todo.id === id;
-    })
+const removeTodo = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id);
 
     if (todoIndex > -1) {
         todos.splice(todoIndex, 1);
@@ -26,21 +24,19 @@ const removeTodo = function(id) {
 }
 
 //Toggle completed value for a todo
-const toggleTodo = function(id) {
-    const todo = todos.find(function(todo) {
-        return todo.id === id;
-    })
+const toggleTodo = (id) => {
+    const todo = todos.find((todo) => todo.id === id)
     if (todo !== undefined) {
-        todo.completed = !todo.completed;
+        todo.completed = !todo.completed
     }
 }
 
 //Render application todos based on filters
-const renderTodos = function(todos, inputFilters) {
-    const filteredTodos = todos.filter(function(todo) {
+const renderTodos = (todos, inputFilters) => {
+    const filteredTodos = todos.filter((todo) => {
     //variable to verify if text input matches todo's text    
         const matchSearchTodos = todo.text.toLowerCase().includes(inputFilters.searchTodos.toLowerCase());
-    //varible using ternary operator to verify whether todo to display matches hide completed checkbox parameters
+    //varible using ternary operator to verify wheer todo to display matches hide completed checkbox parameters
     //in pseudocode: if hide completed checkbox is checked (ie, return completed = false), then return !todo.completed (todo completed = false)
     //else, if checkbox is not checked, return true (ie, return true for all elements searched, display all)
         const matchHideCompleted = inputFilters.hideCompleted ? !todo.completed : true;
@@ -48,20 +44,18 @@ const renderTodos = function(todos, inputFilters) {
         return  matchSearchTodos && matchHideCompleted;
     });
 
-    const todosLeft = filteredTodos.filter(function(todo) {
-        return !todo.completed;
-    });
+    const todosLeft = filteredTodos.filter((todo) => !todo.completed);
     
     document.querySelector('#todo-list').innerHTML = '';
     document.querySelector('#todo-list').appendChild(generateSummaryDOM(todosLeft));
 
-    filteredTodos.forEach(function(todo) {
+    filteredTodos.forEach((todo) => {
         document.querySelector('#todo-list').appendChild(generateTodoDOM(todo));
     });
 }
 
 //Get the DOM elements for an individual note
-const generateTodoDOM = function(todo) {
+const generateTodoDOM = (todo) => {
     const todoElement = document.createElement('div');
     const checkbox = document.createElement('input');
     const todoText = document.createElement('span');
@@ -71,7 +65,7 @@ const generateTodoDOM = function(todo) {
     checkbox.setAttribute('type', 'checkbox');
     checkbox.checked = todo.completed;
     todoElement.appendChild(checkbox);
-    checkbox.addEventListener('change', function() {
+    checkbox.addEventListener('change', () => {
         toggleTodo(todo.id);
         saveTodos(todos);
         renderTodos(todos, inputFilters);
@@ -84,7 +78,7 @@ const generateTodoDOM = function(todo) {
 //Set up the remove button
     removeButton.textContent = 'x';
     todoElement.appendChild(removeButton);
-    removeButton.addEventListener('click', function() {
+    removeButton.addEventListener('click', () => {
         removeTodo(todo.id);
         saveTodos(todos);
         renderTodos(todos, inputFilters);
@@ -94,7 +88,7 @@ const generateTodoDOM = function(todo) {
 }
 
 //Get the DOM elements for list summary
-const generateSummaryDOM = function(todosLeft) {
+const generateSummaryDOM = (todosLeft) => {
     const todosLeftAlert = document.createElement('h2');
     todosLeftAlert.textContent = `You have ${todosLeft.length} todos remaining.`;
     return todosLeftAlert;

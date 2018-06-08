5d8412121d1695bcf7865c8d42016e8ed1949479
Amy Kirasack <amykirasack@gmail.com>
2018-06-07 21:02:23 -0400

    Add truthy falsy definition

---
+++ advanced/truthy-falsy.js
@@ -0,0 +1,8 @@
+const products = [];
+const product = products[0];
+
+//truthy: values that resolve to true in a boolean context
+//falsy: values that resolve to false in a boolean context
+//falsy values: false, 0, '', null, undefined, NaN
+
+console.log(product ? 'Product found' : 'Nothing here!')

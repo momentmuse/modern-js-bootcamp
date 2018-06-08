bd0963a22e0db439dca1ee52d1c4fe26763063a5
Amy Kirasack <amykirasack@gmail.com>
2018-06-07 21:14:48 -0400

    Refactor booleans to truthy or falsy

--- todo-app/todo-functions.js
+++ todo-app/todo-functions.js
@@ -1,7 +1,7 @@
 //Fetch existing todos from localStorage
 const getSavedTodos = () => {
     const todosJSON = localStorage.getItem('todos')
-    return todosJSON !== null ? JSON.parse(todosJSON) : []
+    return todosJSON ? JSON.parse(todosJSON) : []
 }
 
 //Save todos to localStorage
@@ -21,7 +21,7 @@ const removeTodo = (id) => {
 //Toggle completed value for a todo
 const toggleTodo = (id) => {
     const todo = todos.find((todo) => todo.id === id)
-    if (todo !== undefined) {
+    if (todo) {
         todo.completed = !todo.completed
     }
 }

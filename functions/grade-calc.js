00008b2aa5ce6539b4b63db8462ca12dcabced2f
Amy Kirasack <amykirasack@gmail.com>
2018-06-07 23:14:27 -0400

    Add error handling to grade-calc

--- functions/grade-calc.js
+++ functions/grade-calc.js
@@ -4,29 +4,40 @@
 
 //define a function to calculate percentage
 const gradeCalc = function(studentScore = 18, totalScore = 20) {
-    const percent = (studentScore / totalScore) * 100;
-//define letter grade variable    
-    let letterGrade;
-//use if statements to assign letter values from percentage    
-    if(percent >= 90) {
-        letterGrade = 'A';
-    } else if(percent >= 80) {
-        letterGrade = 'B';
-    } else if(percent >= 70) {
-        letterGrade = 'C';
-    } else if(percent >= 60) {
-        letterGrade = 'D';
+    if (typeof studentScore === 'number' && typeof totalScore === 'number') {
+        const percent = (studentScore / totalScore) * 100;
+    //define letter grade variable    
+        let letterGrade;
+    //use if statements to assign letter values from percentage    
+        if(percent >= 90) {
+            letterGrade = 'A';
+        } else if(percent >= 80) {
+            letterGrade = 'B';
+        } else if(percent >= 70) {
+            letterGrade = 'C';
+        } else if(percent >= 60) {
+            letterGrade = 'D';
+        } else {
+            letterGrade = 'F';
+        }
+    //use string template to return grade phrase    
+        return `You got a ${letterGrade} (${percent}%)!`
     } else {
-        letterGrade = 'F';
+        throw Error('Arguments must be numbers!');
     }
-//use string template to return grade phrase    
-    return `You got a ${letterGrade} (${percent}%)!`
 }
 
 //call function with various arguments
 const yourGrade0 = gradeCalc()
 const yourGrade1 = gradeCalc(48, 50)
 const yourGrade2 = gradeCalc(26, 30)
+
+try {
+    const yourGrade3 = gradeCalc('hi', true)
+    console.log(yourGrade3)
+} catch (e) {
+    console.log(`We caught the error! ${e.message}`)
+}
 
 //print function
 console.log('**********GRADE CALCULATOR**********')

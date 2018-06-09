0257d22a72b3cc11a921250203664c28545c1aa2
Amy Kirasack <amykirasack@gmail.com>
2018-06-08 23:44:31 -0400

    Add try catch to todo-app

--- todo-app/todo-functions.js
+++ todo-app/todo-functions.js
@@ -1,7 +1,12 @@
 //Fetch existing todos from localStorage
 const getSavedTodos = () => {
     const todosJSON = localStorage.getItem('todos')
-    return todosJSON ? JSON.parse(todosJSON) : []
+    
+    try {
+        return todosJSON ? JSON.parse(todosJSON) : []
+    } catch (e) {
+        return []
+    }
 }
 
 //Save todos to localStorage

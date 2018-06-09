55ff74ca78af7e3ffd4342c0d6f333a258aab375
Amy Kirasack <amykirasack@gmail.com>
2018-06-09 13:14:57 -0400

    Set up methods for instances of constructor functions

--- oop/person.js
+++ oop/person.js
@@ -1,20 +1,36 @@
-const Person = function (firstName, lastName, age) {
+const Person = function (firstName, lastName, age, likes = []) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
+    this.likes = likes;
 //no need to retun anything because the new operator will create a new object
 }
 
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age}.`
+//arrow function does not bind 'this'
+    this.likes.forEach((like) => {
+//in inner level, 'this' refers to the same value that parent 'this' refers to, ie, const Person
+        bio += ` ${this.firstName} likes ${like}.`
+    });
+    return bio
+}
+
+Person.prototype.setName = function (fullName) {
+     const names = fullName.split(' ');
+     this.firstName = names[0];
+     this.lastName = names[0];
+}
+
 //convention to make constructor function with capital letter
-const me = new Person('Gatsby', 'Fluffykins', 6)
+const cat = new Person('Gatsby', 'Fluffykins', 6, ['chicken', 'sleeping'])
 //functions that get used with new operator are constructor functions
 
 //new operator does a few things:
 //1. generates a new empty object for new instance
 //2. gives us access to that object in the constructor function via 'this' value
+console.log(cat.getBio())
 
-console.log(me)
-
-const person2 = new Person('Clancey', 'Turner', 51)
-
-console.log(person2)
+const person2 = new Person('Clancey', 'Turner', 11)
+person2.setName('Alexis Winter')
+console.log(person2.getBio())

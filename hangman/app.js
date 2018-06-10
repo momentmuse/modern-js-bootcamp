b867db13c3ec2c03c29ce1a16983828652e7ed7b
Amy Kirasack <amykirasack@gmail.com>
2018-06-10 11:21:12 -0400

    Explore prototypal inheritance for arrays, functions, and primitives

--- hangman/app.js
+++ hangman/app.js
@@ -1,10 +1,18 @@
-//product --> Object.prototype --> null
-const product = new Object ({
-    name: 'Eloquent Javascript'
-})
+//Primitive values: string, number, boolean, null, undefined
 
-Object.prototype.someNewMethod = () => 'This is a new method on Objects!'
+//Prototypal Chains (for objects)
+//Object: myObject --> Object.prototype --> null
+//Array: myArray --> Array.prototype --> Object.prototype --> null
+//Function = myFunc --> Function.prototype --> Object.prototype --> null
+//String: myString --> String.prototype --> Object.prototype --> null
+//Number: myNumber --> Number.prototype --> Object.prototype --> null
+//Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
 
-//hasOwnProperty
-console.log(product.someNewMethod());
-console.log(product);
+//String Example
+const product = 'Computer'
+console.log(product)
+
+//When we access a method on a primitive, it converts it to an object ('object wrapper'), and calls the method on object
+
+const otherProduct = new String('iPhone')
+console.log(otherProduct)

876f5c72c7bfe4c597a660dd1e7d5a145dcaa752
Amy Kirasack <amykirasack@gmail.com>
2018-06-11 17:50:06 -0400

    Create DOM elements for hangman mystery word and guesses

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,7 +1,7 @@
  const Answer = function (word, remainingGuesses) {
-    this.word = word.toLowerCase().split('');
-    this.remainingGuesses = remainingGuesses;
-    this.guessedLetters = [];
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.guessedLetters = []
 }
 
 Answer.prototype.getPuzzle = function () {
@@ -9,9 +9,9 @@ Answer.prototype.getPuzzle = function () {
 
     this.word.forEach((letter) => {
         if (this.guessedLetters.includes(letter) || letter === ' ') {
-            puzzle += letter;
+            puzzle += letter
         } else {
-            puzzle += '*';
+            puzzle += '*'
         }
     })
 
@@ -20,8 +20,8 @@ Answer.prototype.getPuzzle = function () {
 
 Answer.prototype.makeGuess = function (guess) {
     guess = guess.toLowerCase();
-    const uniqueGuess = !this.guessedLetters.includes(guess);
-    const badGuess = !this.word.includes(guess);
+    const uniqueGuess = !this.guessedLetters.includes(guess)
+    const badGuess = !this.word.includes(guess)
     if (uniqueGuess) {
         this.guessedLetters.push(guess)
     }
@@ -31,7 +31,12 @@ Answer.prototype.makeGuess = function (guess) {
     }
 }
 
-const game1 = new Answer('cat', 3)
+const puzzleEl = document.querySelector('#puzzle')
+const guessesEl = document.querySelector('#guesses')
+const game1 = new Answer('pineapple', 5)
+
+puzzleEl.textContent = game1.getPuzzle()
+guessesEl.textContent = `${game1.remainingGuesses} guesses left`
 
 console.log(game1.getPuzzle())
 console.log(game1.remainingGuesses)
@@ -40,6 +45,6 @@ window.addEventListener('keypress', function(e) {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
 
-    console.log(game1.getPuzzle())
-    console.log(game1.remainingGuesses)
+    puzzleEl.textContent = game1.getPuzzle()
+    guessesEl.textContent = `${game1.remainingGuesses} guesses left`
 })

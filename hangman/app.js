07b3f20c97dc0d928ca77d34939c8eb7b3f4a2e4
Amy Kirasack <amykirasack@gmail.com>
2018-06-11 18:44:49 -0400

    Add status property and getGameStatus method to Answer prototype, log status to console when playing

--- hangman/app.js
+++ hangman/app.js
@@ -15,23 +15,3 @@ window.addEventListener('keypress', function(e) {
     puzzleEl.textContent = game1.getPuzzle();
     guessesEl.textContent = `${game1.remainingGuesses} guesses left`;
 })
-
-
-// //Primitive values: string, number, boolean, null, undefined
-
-// //Prototypal Chains (for objects)
-// //Object: myObject --> Object.prototype --> null
-// //Array: myArray --> Array.prototype --> Object.prototype --> null
-// //Function = myFunc --> Function.prototype --> Object.prototype --> null
-// //String: myString --> String.prototype --> Object.prototype --> null
-// //Number: myNumber --> Number.prototype --> Object.prototype --> null
-// //Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
-
-// //String Example
-// const product = 'Computer'
-// console.log(product)
-
-// //When we access a method on a primitive, it converts it to an object ('object wrapper'), and calls the method on object
-
-// const otherProduct = new String('iPhone')
-// console.log(otherProduct)
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -2,6 +2,37 @@
     this.word = word.toLowerCase().split('');
     this.remainingGuesses = remainingGuesses;
     this.guessedLetters = [];
+    this.status = '';
+}
+
+Answer.prototype.getGameStatus = function () {
+ //every Method
+ //evaluates to true if every value in array passes test in curly brackets, else evaluates to false
+ //remember you must return the value (this is shortened arrow function syntax, so curly braces and 'return' keyword are removed)
+    const correct = this.word.every((char) => this.guessedLetters.includes(char))
+
+//  //filter Method
+//     const unguessedLetters = this.word.filter((char) => {
+//         return !this.guessedLetters.includes(char)
+//     })   
+
+//  //forEach Method   
+//     let correct = true;
+//     this.word.forEach((char) => {
+//         if (!this.guessedLetters.includes(char)) {
+//             correct = false;
+//         }
+//     })
+    
+    if (correct) {
+        this.status = 'finished';
+    } else if (this.remainingGuesses <= 0) {
+        this.status = 'failed';
+    } else {
+        this.status = 'playing';
+    }
+
+    console.log(this.status)     
 }
 
 Answer.prototype.getPuzzle = function () {
@@ -29,22 +60,5 @@ Answer.prototype.makeGuess = function (guess) {
     if (uniqueGuess && badGuess) {
         this.remainingGuesses--;
     }
+    this.getGameStatus();
 }
-
-const puzzleEl = document.querySelector('#puzzle');
-const guessesEl = document.querySelector('#guesses');
-const game1 = new Answer('pineapple', 5);
-
-puzzleEl.textContent = game1.getPuzzle();
-guessesEl.textContent = `${game1.remainingGuesses} guesses left`;
-
-console.log(game1.getPuzzle());
-console.log(game1.remainingGuesses);
-
-window.addEventListener('keypress', function(e) {
-    const guess = String.fromCharCode(e.charCode);
-    game1.makeGuess(guess);
-
-    puzzleEl.textContent = game1.getPuzzle();
-    guessesEl.textContent = `${game1.remainingGuesses} guesses left`;
-})

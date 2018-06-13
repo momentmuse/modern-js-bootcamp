cfd9e40718fb335806524d4a761d491b772f17ab
Amy Kirasack <amykirasack@gmail.com>
2018-06-12 21:14:56 -0400

    Learn about HTTP Requests, Responses, more JSON parsing

--- hangman/app.js
+++ hangman/app.js
@@ -1,3 +1,7 @@
+//HTTP = Hypertext Transfer Protocol Secure
+//Request: What we want to do
+//Response: What was actually done
+
 const puzzleEl = document.querySelector('#puzzle');
 const guessesEl = document.querySelector('#guesses');
 const game1 = new Answer('pineapple cat', 5);
@@ -15,3 +19,17 @@ window.addEventListener('keypress', function(e) {
     puzzleEl.textContent = game1.puzzle;
     guessesEl.textContent = game1.statusMessage;
 })
+
+//Making an HTTP request
+//the name of this constructor function (XMLHttpRequest) is irrelevant; you can use it to transfer any type of data you want, including JSON data
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data);
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle');
+request.send();

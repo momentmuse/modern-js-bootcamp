16706c9597842cc221365c8703a214553a61d55a
Amy Kirasack <amykirasack@gmail.com>
2018-06-13 22:21:21 -0400

    Practice callback extraction with country name

--- hangman/app.js
+++ hangman/app.js
@@ -22,25 +22,16 @@ window.addEventListener('keypress', (e) => {
 
 getPuzzle((error, puzzle) => {
     if (error) {
-        console.log(`Error: ${error}`)
+        console.log(`Error: ${error}`);
     } else {
-        console.log(puzzle)
+        console.log(puzzle);
     }
 })
 
-
-// const countryCode = "CA";
-// const countryRequest = new XMLHttpRequest();
-
-// countryRequest.addEventListener('readystatechange', (e) => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText);
-//         const country = data.find((country) => country.alpha2Code === countryCode);
-//         console.log(country.name);
-//     } else if (e.target.readyState === 4) {
-//         console.log('Unable to fetch data');
-//     }
-// })
-
-// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
-// countryRequest.send();
+getCountry('CA', (error, country) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+    } else {
+        console.log(`Country name is: ${country.name}`);
+    }
+})
--- hangman/requests.js
+++ hangman/requests.js
@@ -13,3 +13,20 @@ const getPuzzle = (callback) => {
     request.open('GET', 'http://puzzle.mead.io/puzzle');
     request.send();
 }
+
+const getCountry = (countryCode, callback) => {
+    const countryRequest = new XMLHttpRequest();
+
+    countryRequest.addEventListener('readystatechange', (e) => {
+        if (e.target.readyState === 4 && e.target.status === 200) {
+            const data = JSON.parse(e.target.responseText);
+            const country = data.find((country) => country.alpha2Code === countryCode);
+            callback(undefined, country);
+        } else if (e.target.readyState === 4) {
+            callback('There was an error', undefined)
+        }
+    })
+    
+    countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
+    countryRequest.send();
+}

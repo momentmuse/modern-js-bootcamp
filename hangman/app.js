fe120bc2026184d9615449d26b0e2c3160145544
Amy Kirasack <amykirasack@gmail.com>
2018-06-13 20:40:08 -0400

    Practice HTTP requests

--- hangman/app.js
+++ hangman/app.js
@@ -35,3 +35,17 @@ request.addEventListener('readystatechange', (e) => {
 
 request.open('GET', 'http://puzzle.mead.io/puzzle');
 request.send();
+
+const countryCode = "CA";
+const countryRequest = new XMLHttpRequest();
+
+countryRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText);
+        const country = data.find((country) => country.alpha2Code === countryCode);
+        console.log(country.name);
+    }
+})
+
+countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
+countryRequest.send();

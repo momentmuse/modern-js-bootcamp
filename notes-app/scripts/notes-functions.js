3188c19ddc59c3bce730b6d6379f1daa27274009
Amy Kirasack <amykirasack@gmail.com>
2018-06-18 22:46:17 -0400

    Finish styling notes app with CSS, some changes to script to add last edited

--- notes-app/scripts/notes-functions.js
+++ notes-app/scripts/notes-functions.js
@@ -29,18 +29,9 @@ const removeNote = (id) => {
 //Generate the DOM structure for a note
 //argument note will be passed in from the notes-app.js file
 const generateNoteDOM = (note) => {
-    const noteElement = document.createElement('div');
-    const textElement = document.createElement('a')
-    const button = document.createElement('button');
-
-//Set up remove note button
-    button.textContent = 'x'
-    noteElement.appendChild(button)
-    button.addEventListener('click', () => {
-        removeNote(note.id);
-        saveNotes(notes);
-        renderNotes(notes, inputFilters);
-    });
+    const noteElement = document.createElement('a');
+    const textElement = document.createElement('p');
+    const statusElement = document.createElement('p');
 
 //Set up note title text
 //changed from: (note.title.length > 0)
@@ -49,8 +40,17 @@ const generateNoteDOM = (note) => {
     } else {
         textElement.textContent = 'Unnamed note'
     }
-    textElement.setAttribute('href', `/edit.html#${note.id}`)
-    noteElement.appendChild(textElement)
+    textElement.classList.add('list-item__title');
+    noteElement.appendChild(textElement);
+
+//Setup the link
+    noteElement.setAttribute('href', `/edit.html#${note.id}`);
+    noteElement.classList.add('list-item');
+
+//Setup the status message
+    statusElement.textContent = generateLastEdited(note.updatedAt);
+    statusElement.classList.add('list-item__subtitle');
+    noteElement.appendChild(statusElement);
 
     return noteElement
 }
@@ -92,15 +92,23 @@ const sortNotes = (notes, sortBy) => {
 
 //Render application notes
 const renderNotes = (notes, inputFilters) => {
+    const notesEl = document.querySelector('#notes');
     notes = sortNotes(notes, inputFilters.sortBy);
     const filteredNotes = notes.filter((note) => note.title.toLowerCase().includes(inputFilters.searchText.toLowerCase()))
 
-    document.querySelector('#notes').innerHTML = '';
+    notesEl.innerHTML = '';
 
-    filteredNotes.forEach((note) => {
-        const noteElement = generateNoteDOM(note);
-        document.querySelector('#notes').appendChild(noteElement);
-    })
+    if (filteredNotes.length > 0) {
+        filteredNotes.forEach((note) => {
+            const noteElement = generateNoteDOM(note);
+            notesEl.appendChild(noteElement);
+        })
+    } else {
+        const emptyMessage = document.createElement('p');
+        emptyMessage.textContent = 'You have no notes!';
+        emptyMessage.classList.add('empty-message')
+        notesEl.appendChild(emptyMessage);
+    }
 }
 
 const generateLastEdited = (timestamp) => `Last edited ${moment(timestamp).fromNow()}`;

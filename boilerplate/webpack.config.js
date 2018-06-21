09243225465f8b5fd3ba80abeb616ec1d5876308
Amy Kirasack <amykirasack@gmail.com>
2018-06-21 18:35:06 -0400

    Integrate babel with webpack. Uninstalled babel globally and reinstalled it locally

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'public/scripts'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        }]
     }
 }

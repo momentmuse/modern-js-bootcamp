a05140d2f7e86e25529359543b2ea25473913d34
Amy Kirasack <amykirasack@gmail.com>
2018-07-04 00:09:43 -0400

    Update views.js with refactoried code from todo-functions.js old file

--- todo-app/source/views.js
+++ todo-app/source/views.js
@@ -1,13 +1,76 @@
-// renderTodos
-// Arguments: none
-// Return value: none
+import { getTodos, toggleTodo, removeTodo } from './todos';
+import { getFilters } from './filters';
 
-// generateTodoDOM
-// Arguments: todo
-// Return value: the todo element
+const renderTodos = () => {
+    const todoListElement = document.querySelector('#todo-list')
+    const filtrs = getFilters();
+    const filteredTodos = getTodos().filter((todo) => {
+        const matchSearchTodos = todo.text.toLowerCase().includes(filters.searchTodos.toLowerCase());
+        const matchHideCompleted = filters.hideCompleted ? !todo.completed : true;
+        return  matchSearchTodos && matchHideCompleted;
+    });
 
-// generateSummaryDOM
-// Arguments: incompletedTodos
-// Return value: the summary element
+    const todosLeft = filteredTodos.filter((todo) => !todo.completed);
+    
+    todoListElement.innerHTML = '';
+    todoListElement.appendChild(generateSummaryDOM(todosLeft));
 
-// Make sure to set up the exports
+    if (filteredTodos.length > 0) {
+        filteredTodos.forEach((todo) => {
+            todoListElement.appendChild(generateTodoDOM(todo));
+        });
+    } else {
+        const messageElement = document.createElement('p');
+        messageElement.classList.add('empty-message');
+        messageElement.textContent = "You have no todos left. Cool!";
+        todoListElement.appendChild(messageElement);
+    }
+}
+
+
+const generateTodoDOM = (todo) => {
+    const todoElement = document.createElement('label');
+    const containerElement = document.createElement('div');
+    const checkbox = document.createElement('input');
+    const todoText = document.createElement('span');
+    const removeButton = document.createElement('button');
+
+//Set up todo checkbox
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.checked = todo.completed;
+    containerElement.appendChild(checkbox);
+    checkbox.addEventListener('change', () => {
+        toggleTodo(todo.id);
+        renderTodos();
+    })
+
+//Set up the todo text
+    todoText.textContent = todo.text;
+    containerElement.appendChild(todoText);
+
+//Set up container
+    todoElement.classList.add('list-item');
+    containerElement.classList.add('list-item__container');
+    todoElement.appendChild(containerElement);
+
+//Set up the remove button
+    removeButton.textContent = 'delete';
+    removeButton.classList.add('button', 'button--text');
+    todoElement.appendChild(removeButton);
+    removeButton.addEventListener('click', () => {
+        removeTodo(todo.id);
+        renderTodos();
+    })
+
+    return todoElement;
+}
+
+const generateSummaryDOM = (todosLeft) => {
+    const todosLeftAlert = document.createElement('h2');
+    const plural = todosLeft.length === 1 ? '' : 's';
+    todosLeftAlert.classList.add('list-title');
+    todosLeftAlert.textContent = `You have ${todosLeft.length} todo${plural} remaining.`;
+    return todosLeftAlert;
+}
+
+export { generateTodoDOM, renderTodos, generateSummaryDOM };

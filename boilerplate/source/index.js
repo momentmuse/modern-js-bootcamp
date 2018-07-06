f6f55b4859e9f711a74a4371f1f60405216d86f2
Amy Kirasack <amykirasack@gmail.com>
2018-07-05 21:30:01 -0400

    Learn spread operator and practice with simple functions

--- boilerplate/source/index.js
+++ boilerplate/source/index.js
@@ -1,5 +1,5 @@
 //The REST Parameter!
-
+//the rest parameter allows you to represent an indefinite number of arguments as an array
 const calculateAvg = (avgValue, ...numbers) => {
     //return (numOne + numTwo) / 2;
     let sum = 0;
@@ -10,10 +10,30 @@ const calculateAvg = (avgValue, ...numbers) => {
 
 console.log(calculateAvg('grade', 0, 100, 88, 64))
 
+
 const printTeam = (teamName, coachName, ...players) => {
     console.log(`Team: ${teamName}`);
     console.log(`Coach: ${coachName}`);
     console.log(`Players: ${players.join(', ')}`);
 }
 
-printTeam('Liberty', 'Casey Penn', 'Marge', 'Aiden', 'Herbert', 'Sherry')
+//What if this is the input we have to work with, and we can't change the function?
+const team = {
+    name: 'Liberty',
+    coach: 'Casey Penn',
+    players: ['Marge', 'Aiden', 'Herbert', 'Sherry']
+}
+
+//The SPREAD operator!
+//Spread opertor allows you to spread out an array into individual arguments
+printTeam(team.name, team.coach, ...team.players);
+
+//These two are the same
+const cities = ['Barcelona', 'San Francisco', 'Vancouver']
+const citiesSpreadCopy = [...cities]
+
+//add stuff to the copy!
+citiesSpreadCopy.push('Tokyo');
+
+//another way to add stuff
+const citiesSpreadCopy2 = [...cities, 'Tokyo'] //just like .push()
